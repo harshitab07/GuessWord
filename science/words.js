0b01728c7db55a4521f7341a370f7144347d8b0e
Harshita Bhatt <95317276+harshitab07@users.noreply.github.com>
2023-01-31 00:17:48 +0530

    Add files via upload

---
+++ science/words.js
@@ -0,0 +1,66 @@
+let wordList = [
+    {
+        word: "mercury",
+        hint: "Metal liquid at room temperature"
+    },
+    {
+        word: "gold",
+        hint: "Metal with symbol Au"
+    },
+    {
+        word: "lead",
+        hint: "Metal with symbol Pb"
+    },
+    {
+        word: "everest",
+        hint: "Highest mountain peak"
+    },
+    {
+        word: "pacific",
+        hint: "Deepest ocean"
+    },
+    {
+        word: "Frog",
+        hint: "A green coloured amphibian"
+    },
+    {
+        word: "citric",
+        hint: "Sour acid"
+    },
+    {
+        word: "carbon",
+        hint: "Strong bond non-metal"
+    },
+    {
+        word: "sodium",
+        hint: "A metal that knief can cut"
+    },
+    {
+        word: "hydrogen",
+        hint: "Gas used in rockets"
+    },
+    {
+        word: "Helium",
+        hint: "Gas present in Sun"
+    },
+    {
+        word: "neon",
+        hint: "A color and a nobel gas"
+    },
+    {
+        word: "velocity",
+        hint: "Speed with direction"
+    },
+    {
+        word: "air",
+        hint: "State of matter"
+    },
+    {
+        word: "newton",
+        hint: "Discovered gravity"
+    },
+    {
+        word: "starch",
+        hint: "Yellow colour with Iodine"
+    },
+]
