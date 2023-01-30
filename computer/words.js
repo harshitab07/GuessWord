14640e309d6a54842dc4907634aca0870520a5c0
Harshita Bhatt <95317276+harshitab07@users.noreply.github.com>
2023-01-31 00:18:31 +0530

    Add files via upload

---
+++ computer/words.js
@@ -0,0 +1,66 @@
+let wordList = [
+    {
+        word: "python",
+        hint: "A programming language"
+    },
+    {
+        word: "rom",
+        hint: "Type of memory"
+    },
+    {
+        word: "gif",
+        hint: "A file format for image"
+    },
+    {
+        word: "github",
+        hint: "Code hosting platform"
+    },
+    {
+        word: "png",
+        hint: "An image file format"
+    },
+    {
+        word: "mobile",
+        hint: "An electronic device"
+    },
+    {
+        word: "gpu",
+        hint: "A computer component"
+    },
+    {
+        word: "java",
+        hint: "A programming language"
+    },
+    {
+        word: "google",
+        hint: "A famous search engine"
+    },
+    {
+        word: "excel",
+        hint: "A microsoft product for windows"
+    },
+    {
+        word: "mysql",
+        hint: "A relational database system"
+    },
+    {
+        word: "proxy",
+        hint: "Related to server application"
+    },
+    {
+        word: "html",
+        hint: "Markup language for the web"
+    },
+    {
+        word: "point",
+        hint: "Microsoft Power _"
+    },
+    {
+        word: "svg",
+        hint: "A vector image format"
+    },
+    {
+        word: "jpeg",
+        hint: "A image file format"
+    },
+]
