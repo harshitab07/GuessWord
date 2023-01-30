b42171390e4ea57cd531a1933558b2216ee50e82
Harshita Bhatt <95317276+harshitab07@users.noreply.github.com>
2023-01-31 00:19:41 +0530

    Add files via upload

---
+++ sports/words.js
@@ -0,0 +1,66 @@
+let wordList = [
+    {
+        word: "eleven",
+        hint: "No. of players in cricket"
+    },
+    {
+        word: "dunk",
+        hint: "Basketball move"
+    },
+    {
+        word: "soccer",
+        hint: "American football name"
+    },
+    {
+        word: "baseball",
+        hint: "Kind of cricket"
+    },
+    {
+        word: "boxer",
+        hint: "Mohammad ali"
+    },
+    {
+        word: "lionel",
+        hint: "Messi's first name"
+    },
+    {
+        word: "france",
+        hint: "Mbappe home country"
+    },
+    {
+        word: "yellow",
+        hint: "Colour of a tennis ball"
+    },
+    {
+        word: "msd",
+        hint: "Bole jo koyal baago mai"
+    },
+    {
+        word: "usain",
+        hint: "Greatest sprinter of all time"
+    },
+    {
+        word: "brazil",
+        hint: "Neymar is from?"
+    },
+    {
+        word: "gambhir",
+        hint: "Gautam?"
+    },
+    {
+        word: "hockey",
+        hint: "Indian national sport"
+    },
+    {
+        word: "eleven",
+        hint: "Players in football"
+    },
+    {
+        word: "five",
+        hint: "Players in basketball"
+    },
+    {
+        word: "six",
+        hint: "Players in volleyball"
+    },
+]
