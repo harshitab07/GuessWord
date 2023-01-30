9fa084e22a1f148b39a7eb1fb2c048962ef92ef0
Harshita Bhatt <95317276+harshitab07@users.noreply.github.com>
2023-01-31 00:17:08 +0530

    Add files via upload

---
+++ bolywood/words.js
@@ -0,0 +1,66 @@
+let wordList = [
+    {
+        word: "amitabh",
+        hint: "Big B of Bollywood"
+    },
+    {
+        word: "mannat",
+        hint: "Srk's home"
+    },
+    {
+        word: "ranveer",
+        hint: "Band Baja Barrat"
+    },
+    {
+        word: "alia",
+        hint: "Raazi actress"
+    },
+    {
+        word: "arjun",
+        hint: "Karan - "
+    },
+    {
+        word: "karan",
+        hint: "A famous director"
+    },
+    {
+        word: "srk",
+        hint: "King khan"
+    },
+    {
+        word: "amir",
+        hint: "3 idiots hero"
+    },
+    {
+        word: "pathan",
+        hint: "SRK new movie"
+    },
+    {
+        word: "chadda",
+        hint: "Lal Singh __"
+    },
+    {
+        word: "srk",
+        hint: "DDLJ"
+    },
+    {
+        word: "salman",
+        hint: "Prem naam to suna hoga"
+    },
+    {
+        word: "akshay",
+        hint: "Boss is always right"
+    },
+    {
+        word: "heroin",
+        hint: "Common between alia, deepika, katrina"
+    },
+    {
+        word: "sallu",
+        hint: "Rash driver"
+    },
+    {
+        word: "gauri",
+        hint: "SRK wife"
+    },
+]
